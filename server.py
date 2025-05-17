8d76cd4f7467c51618a285fbc845ebd89ce30055
lteixeiraufg <lucas.teixeira@discente.ufg.br>
2025-05-17 15:21:26 -0300

    Update server.py

--- server.py
+++ server.py
@@ -1,18 +1,55 @@
+# import rpyc
+# from constRPYC import * #-
+# from rpyc.utils.server import ThreadedServer
+
+# class DBList(rpyc.Service):
+#   value = []
+
+#   def exposed_append(self, data):
+#     self.value = self.value + [data]
+#     return self.value
+
+#   def exposed_value(self):
+#     return self.value
+
+# if __name__ == "__main__":
+#   server = ThreadedServer(DBList(), port = PORT)
+#   server.start()
+
 import rpyc
-from constRPYC import * #-
+from constRPYC import *
 from rpyc.utils.server import ThreadedServer
 
 class DBList(rpyc.Service):
-  value = []
+    value = []
 
-  def exposed_append(self, data):
-    self.value = self.value + [data]
-    return self.value
+    def exposed_append(self, data):
+        self.value = self.value + [data]
+        return self.value
 
-  def exposed_value(self):
-    return self.value
+    def exposed_value(self):
+        return self.value
+
+    def exposed_clear(self):
+        self.value = []
+        return self.value
+
+    def exposed_get(self, index):
+        try:
+            return self.value[index]
+        except IndexError:
+            return f"Erro: índice {index} fora do alcance"
+
+    def exposed_remove(self, val):
+        try:
+            self.value.remove(val)
+            return self.value
+        except ValueError:
+            return f"Erro: valor {val} não encontrado na lista"
+
+    def exposed_length(self):
+        return len(self.value)
 
 if __name__ == "__main__":
-  server = ThreadedServer(DBList(), port = PORT)
-  server.start()
-
+    server = ThreadedServer(DBList(), port=PORT)
+    server.start()
